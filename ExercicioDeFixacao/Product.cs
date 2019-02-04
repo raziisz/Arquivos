33c12cd2c305e4cd3ab4127c6de2c113c555621c
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-04 18:18:19 -0400

    Feito exercicio de fixacao com arquivos e usado suas funcoes

---
+++ ExercicioDeFixacao/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDeFixacao
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Preco { get;  set; }
+        public int Quantidade { get; set; }
+
+        public Product(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double Total()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ ExercicioDeFixacao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ExercicioDeFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(@"Entre com o caminho do arquivo: ");
+            string file = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(file);
+
+                string sourceFolderPath = Path.GetDirectoryName(file);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (var line in lines)
+                    {
+                        string[] saida = line.Split(',');
+                        string name = saida[0];
+                        double valor = double.Parse(saida[1], CultureInfo.InvariantCulture);
+                        int qtd = int.Parse(saida[2]);
+
+                        Product pro = new Product(name, valor, qtd);
+
+
+                        sw.WriteLine(pro.Nome + ", " + pro.Total().ToString("F2",CultureInfo.InvariantCulture));
+
+                    }
+                }
+
+                using (StreamReader sr = File.OpenText(targetFilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string linha = sr.ReadLine();
+                        Console.WriteLine(linha);
+                    }
+                }
+                
+            }catch (IOException e)
+            {
+                Console.WriteLine("Houve um erro");
+                Console.WriteLine(e.Message);
+
+
+            }
+        }
+    }
+}
