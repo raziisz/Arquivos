5abf48cb44acac2a8915b3e7bbd5d06fc12be1dd
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-04 17:14:48 -0400

    Mexendo com path

--- File_FileInfo/Program.cs
+++ File_FileInfo/Program.cs
@@ -91,6 +91,7 @@ namespace File_FileInfo
                 Console.WriteLine(e.Message);
             } */
 
+            /*
             string path = @"c:\Projetos\myfolder";
 
             try
@@ -116,7 +117,18 @@ namespace File_FileInfo
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
+            */
 
+            string path = @"c:\Projetos\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: "+Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: "+Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: "+ Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: "+Path.GetFileName(path));
+            Console.WriteLine("GetExtension: "+Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: "+Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: "+Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: "+Path.GetTempPath());
 
         }
     }
