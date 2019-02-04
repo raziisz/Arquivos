a28dc6ec47689e18bd9a5e5547cf50231e7673ff
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-04 16:24:16 -0400

    Mechendo com fileStream e StreamReader

--- File_FileInfo/Program.cs
+++ File_FileInfo/Program.cs
@@ -7,7 +7,8 @@ namespace File_FileInfo
     {
         static void Main(string[] args)
         {
-            
+
+            /*
             string sourcePath = @"c:\Projetos\file1.txt";
             string targetPath = @"c:\Projetos\file2.txt";
             
@@ -26,6 +27,32 @@ namespace File_FileInfo
             {
                 Console.WriteLine("An error occrurred");
                 Console.WriteLine(e.Message);
+            }
+            */
+
+            string path = @"c:\Projetos\file1.txt";
+           
+            StreamReader sr = null;
+
+            try
+            {
+
+                sr = File.OpenText(path);
+                while(!sr.EndOfStream){
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                if (sr != null) sr.Close();
+                
             }
 
         }
